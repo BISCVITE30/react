69ba82347fb71d71f522a3eb6c103e0d5299f980
BISCVITE30 <gonnki2004@gmail.com>
2024-06-14 21:27:01 +0200

    !

--- lesson5-events/task1/src/GoodButton.jsx
+++ lesson5-events/task1/src/GoodButton.jsx
@@ -2,9 +2,12 @@ import React, { Component } from "react";
 import './index.scss'
 
 class GoodButton extends Component {
+    clickOn(){
+        alert('You Click')
+    }
     render() {
         return(
-            <button className="fancy-button">Click me!</button>
+            <button className="fancy-button" onClick={this.clickOn}>Click me!</button>
         )
     }
 }
