0efb9719c109dad3883296c49bdc7da2a9e4283a
BISCVITE30 <gonnki2004@gmail.com>
2024-06-14 21:18:15 +0200

    !

---
+++ lesson5-events/task1/src/GoodButton.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import './index.scss'
+
+class GoodButton extends Component {
+    render() {
+        return(
+            <button className="fancy-button">Click me!</button>
+        )
+    }
+}
+
+export default GoodButton;
