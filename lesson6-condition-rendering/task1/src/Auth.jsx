26ac4a2c81522e89008142497a3b8eb6537399a9
BISCVITE30 <gonnki2004@gmail.com>
2024-06-16 11:42:02 +0200

    !

---
+++ lesson6-condition-rendering/task1/src/Auth.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import Greeting from './Greeting.jsx';
+import './index.scss';
+import Login from './Login.jsx';
+import Logout from './Logout.jsx';
+
+class Auth extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoggedIn: false,
+    };
+  }
+
+  handleLogout() {
+    this.setState({
+      isLoggedIn: false,
+    });
+  }
+
+  handleLogin() {
+    this.setState({
+      isLoggedIn: true,
+    });
+  }
+
+  render() {
+    let button;
+
+    if (this.state.isLoggedIn) {
+        button = <button className='btn login' onClick={() => this.handleLogout()}><Login /></button>;
+    } else {
+        button = (
+          <button className="btn logout" onClick={() => this.handleLogin()}>
+            <Logout />
+          </button>
+        );
+  
+    }
+
+    return (
+      <div className="panel">
+        <Greeting isLoggedIn={this.state.isLoggedIn} />
+        <div>{button}</div>
+      </div>
+    );
+  }
+}
+
+export default Auth;
