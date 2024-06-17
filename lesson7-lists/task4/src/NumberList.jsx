6dce864755d9a359a0e070b460aad5b2fa113f09
BISCVITE30 <gonnki2004@gmail.com>
2024-06-17 13:21:19 +0200

    !

---
+++ lesson7-lists/task4/src/NumberList.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+class NumberList extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            numbers: props.numbers,
+        }
+    }
+
+    render(){
+        return (
+          <ul>
+            <li>{this.state.numbers[0]}</li>
+            <li>{this.state.numbers[1]}</li>
+            <li>{this.state.numbers[2]}</li>
+            <li>{this.state.numbers[3]}</li>
+            <li>{this.state.numbers[4]}</li>
+          </ul>
+        );
+    }
+}
+
+export default NumberList;
