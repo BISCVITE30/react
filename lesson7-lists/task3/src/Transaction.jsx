9a8795fc8340c9b72f84a2574ab064e29614aa59
BISCVITE30 <gonnki2004@gmail.com>
2024-06-17 16:59:57 +0200

    !

--- lesson7-lists/task3/src/Transaction.jsx
+++ lesson7-lists/task3/src/Transaction.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import moment from 'moment';
 
-
 const formatDate = date => moment(date).format('DD MMM');
 const formatHour = date => moment(date).format('HH:MM');
 
 const Transaction = ({ from, to, amount, rate, time, id }) => {
-    console.log(formatHour(new Date(time)));
   return (
     <li key={id} className="transaction">
       <span className="transaction__date">{formatDate(time)}</span>
       <span className="transaction__time">{formatHour(time)}</span>
-      <span className="transaction__assets">{from} → {to}</span>
-      <span className="transaction__rate">{rate}</span>
-      <span className="transaction__amount">{amount}</span>
+      <span className="transaction__assets">
+        {from} → {to}
+      </span>
+      <span className="transaction__rate">{new Intl.NumberFormat('en-GB').format(rate)}</span>
+      <span className="transaction__amount">{new Intl.NumberFormat('en-GB').format(amount)}</span>
     </li>
   );
 };
