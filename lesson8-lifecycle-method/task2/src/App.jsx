87f9c6ffca2db880b0c24e92cb455684aa538261
BISCVITE30 <gonnki2004@gmail.com>
2024-06-19 17:34:13 +0200

    !

--- lesson8-lifecycle-method/task2/src/App.jsx
+++ lesson8-lifecycle-method/task2/src/App.jsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Clock from './Clock.jsx';
 
-const App = () => {
-  return <div>Hello, React!</div>;
-};
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      visible: true,
+    };
+
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    this.setState({
+      visible: !this.state.visible,
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <button onClick={this.toggle}>toggle</button>
+        <div>{this.state.visible && <Clock location="New York" offset={-5} />}</div>
+        <div>{this.state.visible && <Clock location="Kyiv" offset={2} />}</div>
+        <div>{this.state.visible && <Clock location="London" offset={0} />}</div>
+      </>
+    );
+  }
+}
 
 export default App;
---
+++ lesson8-lifecycle-method/task2/src/Clock.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { getTimeWithOffset } from './getTimeWithOffset';
+import moment from 'moment';
+
+const formatDate = date => moment(date).format('h:mm:ss A');
+
+class Clock extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentTime: formatDate(getTimeWithOffset(this.props.offset)),
+    };
+  }
+
+  componentDidMount() {
+    this.intervar = setInterval(() => {
+      this.setState({
+        currentTime: formatDate(getTimeWithOffset(this.props.offset)),
+      });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervar)
+  }
+
+  render() {
+    return (
+      <div className="clock">
+        <div className="clock__location">{this.props.location}</div>
+        <div className="clock__time">{this.state.currentTime}</div>
+      </div>
+    );
+  }
+}
+
+export default Clock;
