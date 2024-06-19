418cbd7b69e45cfc802859f468e64810c4632bd4
BISCVITE30 <gonnki2004@gmail.com>
2024-06-19 21:59:34 +0200

    !

---
+++ lesson8-lifecycle-method/task5/src/ConnectionStatus.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+class ConnectionStatus extends Component {
+  constructor() {
+    super();
+    this.state = {
+      status: null,
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      status: 'online',
+    });
+
+    window.addEventListener('online', this.handleOnline);
+    window.addEventListener('offline', this.handleOffline);
+  }
+
+  handleOnline = () => {
+    this.setState({
+      status: 'online',
+    });
+  };
+
+  handleOffline = () => {
+    this.setState({
+      status: 'offline',
+    });
+  };
+
+  render() {
+    // return this.state.status === 'online' ? this.handleOnline() : this.handleOffline;
+    return (
+      <div className={`status ${this.state.status === 'offline' ? 'status_offline' : ''}`}>
+        {this.state.status}
+      </div>
+    );
+  }
+}
+
+export default ConnectionStatus;
