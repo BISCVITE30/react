a698f55e21f473b1828aec21d783a61f782b3d16
BISCVITE30 <gonnki2004@gmail.com>
2024-06-19 11:41:46 +0200

    !

--- lesson8-lifecycle-method/task1/src/Life.jsx
+++ lesson8-lifecycle-method/task1/src/Life.jsx
@@ -7,28 +7,24 @@ class Life extends Component {
   }
 
   componentDidMount() {
-    return console.log('componentDidMount: API calls, subscriptions');
+    console.log('componentDidMount: API calls, subscriptions');
   }
 
-  shouldComponentUpdate(nextProps, nextState) {
-   return console.log(
-     'shouldComponentUpdate(nextProps, nextState): decide to render or not to render',
-   );
+  shouldComponentUpdate() {
+    console.log('shouldComponentUpdate(nextProps, nextState): decide to render or not to render');
   }
 
-  componentDidUpdate(prevProps, prevState) {
-    return console.log('componentDidUpdate(prevProps, prevState): some updates based on new props');
+  componentDidUpdate() {
+    console.log('componentDidUpdate(prevProps, prevState): some updates based on new props');
   }
 
   componentWillUnmount() {
-    return console.log(
-      'componentWillUnmount(): cleanup before DOM related to component will be removed',
-    );
+    console.log('componentWillUnmount(): cleanup before DOM related to component will be removed');
   }
 
   render() {
     console.log('return React element to build DOM');
-    return null
+    return null;
   }
 }
 
