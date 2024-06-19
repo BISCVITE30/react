d7bd30a88fae89e4161ee8840d54e3ec2860f6f7
BISCVITE30 <gonnki2004@gmail.com>
2024-06-19 11:36:57 +0200

    !

--- lesson8-lifecycle-method/task1/src/Life.jsx
+++ lesson8-lifecycle-method/task1/src/Life.jsx
@@ -1,30 +1,31 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-class Life extends Component{
-    constructor(){
-        super()
-        console.log('good place to create state');
-    }
+class Life extends Component {
+  constructor() {
+    super();
+    console.log('good place to create state');
+  }
 
-    ComponentDidMount() {
-        return console.log('API calls, subscritions')
-    }
+  ComponentDidMount() {
+    return console.log('API calls, subscritions');
+  }
 
-    shouldComponentUpdate(nextProps, nextState) {
-        return console.log('decide to render not to render')
-    }
+  shouldComponentUpdate(nextProps, nextState) {
+   return console.log('decide to render not to render');
+  }
 
-    ComponentDidUpdate(prevProps, prevState) {
-        return console.log('some updates based on new props')
-    }
+  ComponentDidUpdate(prevProps, prevState) {
+    return console.log('some updates based on new props');
+  }
 
-    ComponentWillUnmount() {
-        return console.log('cleanup before DOM related to component will be removed')
-    }
+  ComponentWillUnmount() {
+    return console.log('cleanup before DOM related to component will be removed');
+  }
 
-    render() {
-        return console.log('return React element to build DOM')
-    }
+  render() {
+    console.log('return React element to build DOM');
+    return null
+  }
 }
 
 export default Life;
