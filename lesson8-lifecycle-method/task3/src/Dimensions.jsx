99500933deaaa2b0c498fc5b7a3d0b85d70530ae
BISCVITE30 <gonnki2004@gmail.com>
2024-06-19 17:52:58 +0200

    !

---
+++ lesson8-lifecycle-method/task3/src/Dimensions.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class Dimensions extends Component {
+    constructor(){
+        super();
+
+        this.state = {
+            width:  window.innerWidth,
+            height: window.innerHeight
+        }
+    }
+
+    componentDidMount(){
+        window.addEventListener('resize', this.onResize)
+    }
+
+    componentWillUnMount(){
+        window.removeEventListener('resize', this.onResize)
+    }
+    
+    onResize = e => {
+        const { innerHeight, innerWidth } = e.target;
+        
+        this.setState({
+            width: innerWidth,
+            height: innerHeight,
+        });
+    }
+    
+  render() {
+    return <div className="dimensions">{this.state.height}px - {this.state.width}px</div>;
+  }
+}
+
+export default Dimensions;
