adfdd2a242b0b558f5b8435de980233297f5b488
BISCVITE30 <gonnki2004@gmail.com>
2024-06-28 22:25:25 +0200

    !

--- lesson10-lifring-state-up/task3/src/App.jsx
+++ lesson10-lifring-state-up/task3/src/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import Calculator from './CalculatorTemprature.jsx';
 
 const App = () => {
-  return <div>Hello, React!</div>;
+  return <Calculator />;
 };
 
 export default App;
