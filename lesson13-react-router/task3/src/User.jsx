f2a63b9614a1d26fe99d6c5389a0dc994a1d5126
BISCVITE30 <gonnki2004@gmail.com>
2024-07-04 10:34:03 +0200

    !

--- lesson13-react-router/task3/src/User.jsx
+++ lesson13-react-router/task3/src/User.jsx
@@ -1,22 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const User = ({ match }) => {
-  console.log(match.url);
-  const fetchUser =  (match) => {
-    return fetch(`https://api.github.com${match.url}`).then(repsonse => repsonse.json())
+const fetchUser = match => {
+  return fetch(`https://api.github.com${match.url}`).then(repsonse => repsonse.json());
+};
+
+class User extends Component {
+  state = {
+    userData: null,
+  };
+
+  componentDidMount() {
+    this.fetchUserData();
   }
 
-  const user = fetchUser(match).then(userData => userData)
+  componentDidUpdate(prevProps) {
+    const prevUser = prevProps.match.params.userId;
+    const currentUser = this.props.match.params.userId;
 
-  return (
-    <div className="user">
-      <img alt="User Avatar" src={user.avatar_url} className="user__avatar" />
-      <div className="user__info">
-        <span className="user__name">GitHub</span>
-        <span className="user__location">San Francisco,CA</span>
+    if (prevUser !== currentUser) {
+      this.fetchUserData();
+    }
+  }
+
+  fetchUserData() {
+    const { match } = this.props;
+    fetch(`https://api.github.com/users/${match.params.userId}`).then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+
+      throw new Error()
+    }).then(userData => {
+      this.setState({
+        userData,
+      })
+    })
+  }
+  render() {
+    const { userData } = this.state;
+    if(!userData){
+      return null;
+    }
+    const { name, location, avatar_url } = userData;
+    return (
+      <div className="user">
+        <img alt="User Avatar" src={avatar_url} className="user__avatar" />
+        <div className="user__info">
+          <span className="user__name">{name}</span>
+          <span className="user__location">{location}</span>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default User;
